8cb5d91f0f25ce32f297f98eaa26f22a922bdff6
Daphne271991 <134757720+Daphne271991@users.noreply.github.com>
2023-10-14 17:15:55 -0600

    Initial commit

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,15 @@
+import { motion } from "framer-motion";
+import "./sidebar.scss";
+
+const Sidebar = () => {
+  return (
+    <motion.div className="sidebar" animate={open ? "open" : "closed"}>
+      <motion.div className="bg" variants={variants}>
+        <Links />
+      </motion.div>
+      <ToggleButton setOpen={setOpen} />
+    </motion.div>
+  );
+};
+
+export default Sidebar;
